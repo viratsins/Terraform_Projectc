af04f65a52f9047639a2442588fc620b84908788
Aditya113 <46567659+viratsins@users.noreply.github.com>
2024-11-30 17:34:46 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,1 @@
-terraform {
-backend "s3" {
-    bucket         = "scalable-infra-bucket-752"
-    dynamodb_table = "state-lock"
-    key            = "infra/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-}
-}
+
